76e21fe1bfc4388ad41e1a00940bdc7ef9bb5338
satishuogithub <134980118+satishuogithub@users.noreply.github.com>
2023-10-10 20:26:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,139 +1,219 @@
+//Here book is used instead of product
+
 const express = require('express');
-
 const app = express();
-
-//app.use(express.json());
-
 const path = require('path');
-
 const sqlite3 = require('sqlite3');
-
-const { open } = require ('sqlite');
-
-const port = 3000;
+const { open } = require('sqlite');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const dbPath = path.join(__dirname, 'bookpractice.db');
 let db = null;
-const initializeDbAndServer = async () =>{
-    try{
+
+const initializeDbAndServer = async () => {
+  try {
     db = await open({
-        filename: dbPath,
-        driver : sqlite3.Database
+      filename: dbPath,
+      driver: sqlite3.Database,
     });
     app.listen(3000, () => {
-        console.log(`Server is listening at port : ${port}`);
+      console.log(`Server is listening at port: ${port}`);
     });
-    } catch(e){
-        console.log(`server error: ${ e.message }`);
-    }
-}
+  } catch (e) {
+    console.log(`Server error: ${e.message}`);
+  }
+};
+
 initializeDbAndServer();
-//get books
-app.get('/books/', async(request, response) =>{
-        const getBooksQuery =`
-            SELECT 
-             * 
+
+//Gets list of all books with a JSON Web Token from data base
+
+app.get('/books/', (request, response) => {
+  let jwtToken;
+  const authHeader = request.headers['authorization'];
+  if (authHeader !== undefined) {
+    jwtToken = authHeader.split(' ')[1];
+  }
+  if (jwtToken === undefined) {
+    response.status(401).send('Invalid Access Token');
+  } else {
+    jwt.verify(jwtToken, 'MY_SECRET_TOKEN', async (error, payload) => {
+      if (error) {
+        response.send('Invalid Access Token');
+      } else {
+        const getBooksQuery = `
+            SELECT
+              *
             FROM
              book
             ORDER BY
-            book_id ;
-        `;
+              book_id;`;
         const booksArray = await db.all(getBooksQuery);
         response.send(booksArray);
+      }
+    });
+  }
 });
 
-//get single book
-app.get('/books/:bookId/', async (request, response) =>{
-    const {bookId} = request.params;
-    const getBookQuery = `SELECT
+//Gets single book details with a JSON Web Token from data base
+
+app.get('/books/:bookId/', (request, response) => {
+  let jwtToken;
+  const authHeader = request.headers['authorization'];
+  if (authHeader !== undefined) {
+    jwtToken = authHeader.split(' ')[1];
+  }
+  if (jwtToken === undefined) {
+    response.status(401).send('Invalid Access Token');
+  } else {
+    jwt.verify(jwtToken, 'MY_SECRET_TOKEN', async (error, payload) => {
+      if (error) {
+        response.send('Invalid Access Token');
+      } else {
+        const { bookId } = request.params;
+        const getBookQuery = `SELECT
                       *
                     FROM 
                     book
                     WHERE 
-                    book_id = ${ bookId };`;
-    const book = await db.get(getBookQuery);
-    response.send( book );
+                    book_id = ${bookId};`;
+        const book = await db.get(getBookQuery);
+        response.send(book);
+      }
+    });
+  }
 });
 
-//post book
-app.post("/addbook/", async (request, response) => {
-    
-    const bookDetails = {
+// Posts book details  with a JSON Web Token 
+
+app.post('/addbook/', (request, response) => {
+  let jwtToken;
+  const authHeader = request.headers['authorization'];
+  if (authHeader !== undefined) {
+    jwtToken = authHeader.split(' ')[1];
+  }
+  if (jwtToken === undefined) {
+    response.status(401).send('Invalid Access Token');
+  } else {
+    jwt.verify(jwtToken, 'MY_SECRET_TOKEN', async (error, payload) => {
+      if (error) {
+        response.send('Invalid Access Token');
+      } else {
+        const bookDetails = {
       
-      book_id: 1,
-      title: 'book10',
-      author_id: 'author10',
-      rating: 4.5,
-      rating_Count: 10000,
-      
-    };
-  
-    
-    const {
-        book_id,
-        title,
-        author_id,
-        rating,
-        rating_Count,
-    } = bookDetails;
-  
-    const addBookQuery = `
-      INSERT INTO
-        book (title,author_id,rating,rating_count,review_count,description,pages,date_of_publication,edition_language,price,online_stores)
-      VALUES
-        (
-           ${book_id},
-           '${title}',
-           ${author_id},
-           ${rating},
-           ${rating_Count},
-           
+            book_id: 1,
+            title: 'book10',
+            author_id: 'author10',
+            rating: 4.5,
+            rating_Count: 10000,
+            
+          };
+        
           
-        );`;
-  
-    const dbResponse = await db.run(addBookQuery);
-    const bookId = dbResponse.lastID;
-  
-    
-    response.send({ bookId: bookId });
-  });
-  //update book
-  app.put("/books/:bookId/", async (request, response) => {
-    const { bookId } = request.params;
-    const bookDetails = request.body;
-    const {
-        book_id,
-        title,
-        author_id,
-        rating,
-        rating_Count,
-    } = bookDetails;
-    const updateBookQuery = `
-      UPDATE
-        book
-      SET
-      ${book_id},
-      '${title}',
-      ${author_id},
-      ${rating},
-      ${rating_Count},
-      WHERE
-        book_id = ${bookId};`;
-    await db.run(updateBookQuery);
-    response.send("Book Updated Successfully");
-  });
-  server1.delete("/books/:bookId/", async (request, response) => {
-    const { bookId } = request.params;
-    const deleteBookQuery = `
-      DELETE FROM
-        book
-      WHERE
-        book_id = ${bookId};`;
-    await db.run(deleteBookQuery);
-    response.send("Book Deleted Successfully");
-  });
-//registeration
-  app.post("/users/", async (request, response) => {
+          const {
+              book_id,
+              title,
+              author_id,
+              rating,
+              rating_Count,
+          } = bookDetails;
+        
+          const addBookQuery = `
+            INSERT INTO
+              book (title,author_id,rating,rating_count)
+            VALUES
+              (
+                 ${book_id},
+                 '${title}',
+                 ${author_id},
+                 ${rating},
+                 ${rating_Count},
+                 
+                
+              );`;
+        
+          const dbResponse = await db.run(addBookQuery);
+          const bookId = dbResponse.lastID;
+        
+          
+          response.send({ bookId: bookId });
+      }
+    });
+  }
+});
+
+// Update a book with a JSON Web Token 
+
+app.put('/books/:bookId/', (request, response) => {
+  let jwtToken;
+  const authHeader = request.headers['authorization'];
+  if (authHeader !== undefined) {
+    jwtToken = authHeader.split(' ')[1];
+  }
+  if (jwtToken === undefined) {
+    response.status(401).send('Invalid Access Token');
+  } else {
+    jwt.verify(jwtToken, 'MY_SECRET_TOKEN', async (error, payload) => {
+      if (error) {
+        response.send('Invalid Access Token');
+      } else {
+        const { bookId } = request.params;
+        const bookDetails = request.body;
+        const {
+            book_id,
+            title,
+            author_id,
+            rating,
+            rating_Count,
+        } = bookDetails;
+        const updateBookQuery = `
+        UPDATE
+            book
+        SET
+        ${book_id},
+        '${title}',
+        ${author_id},
+        ${rating},
+        ${rating_Count},
+        WHERE
+            book_id = ${bookId};`;
+        await db.run(updateBookQuery);
+        response.send("Book Updated Successfully");
+      }
+    });
+  }
+});
+
+// Deletes a book with help of JSON WEB TOKEN
+app.delete('/books/:bookId/', (request, response) => {
+  let jwtToken;
+  const authHeader = request.headers['authorization'];
+  if (authHeader !== undefined) {
+    jwtToken = authHeader.split(' ')[1];
+  }
+  if (jwtToken === undefined) {
+    response.status(401).send('Invalid Access Token');
+  } else {
+    jwt.verify(jwtToken, 'MY_SECRET_TOKEN', async (error, payload) => {
+      if (error) {
+        response.send('Invalid Access Token');
+      } else {
+        const { bookId } = request.params;
+        const deleteBookQuery = `
+        DELETE FROM
+            book
+        WHERE
+            book_id = ${bookId};`;
+        await db.run(deleteBookQuery);
+        response.send("Book Deleted Successfully");
+      }
+    });
+  }
+});
+
+///registeration - Hash code generated with bcrypt and stored in db
+app.post("/users/", async (request, response) => {
     const { username, name, password, gender, location } = request.body;
     const hashedPassword = await bcrypt.hash(request.body.password, 10);
     const selectUserQuery = `SELECT * FROM user WHERE username = '${username}'`;
@@ -158,7 +238,9 @@ app.post("/addbook/", async (request, response) => {
       response.send("User already exists");
     }
   });
-//login authentication
+
+//login -  here hashed password is compared with user password with bcrypt.compare
+
   app.post("/login", async (request, response) => {
     const { username, password } = request.body;
     const selectUserQuery = `SELECT * FROM user WHERE username = '${username}'`;
